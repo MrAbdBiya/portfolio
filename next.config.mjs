d9fb0b499e6385f3041b9b03df86dfbdc106e2c5
Votre Nom <votre.email@example.com>
2024-11-30 17:51:48 +0100

    Add GitHub Pages configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/portfolio',
+  images: {
+    unoptimized: true,
+  },
   transpilePackages: ['three'],
   webpack: (config) => {
     config.externals.push({
